034a1def591c5ac47ae8c60eccec06afd1c12190
cynraw <cynthiaronoh24@gmail.com>
2025-02-03 09:34:57 +0300

    A greeting app

---
+++ greetings.py
@@ -0,0 +1,5 @@
+print("Starting the script...") 
+name = input("Please Enter your name: ")
+color = input("What is your favourite color? ")
+
+print(f"Hello, {name}! Your favorite color, {color}, is awesome!")
