2d1ee760d8f8d6bed41ca6a99105435d58caa111
Cynthia Ronoh <cynthiaronoh24@gmail.com>
2025-03-19 12:07:31 +0300

    Data manipulation with pandas

--- python_libraries/pandas/createDataFrames.py
+++ python_libraries/pandas/createDataFrames.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+data = {
+    'Name': ['Alice', 'Bob', 'Ronoh'],
+    'Age': [23,40,22],
+    'Cities': ['Nairobi', 'Kisumu', 'Eldoret']
+}
+
+df = pd.DataFrame(data)
+print(df)
+print(df[df['Age'] > 30]) # filter rows with age > 30
